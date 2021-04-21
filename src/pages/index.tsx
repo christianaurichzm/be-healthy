5700e21c5103c101c116b30f39fddc503152ac04
Christian Aurich <christian.aurichzm@gmail.com>
2021-04-21 07:51:15 -0300

    Add ExperienceBar component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
+import Head from "next/head";
+
 import { Profile } from "../components/Profile";
+import { ExperienceBar } from "../components/ExperienceBar";
 
 import styles from "../styles/pages/Home.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.homeContainer}>
+      <Head>
+        <title>Be Healthy | Home</title>
+      </Head>
+      <ExperienceBar />
       <Profile />
     </main>
   );
