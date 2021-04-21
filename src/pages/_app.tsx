37c9ab2bdd5908644309e70fb321751e10d61162
Christian Aurich <christian.aurichzm@gmail.com>
2021-04-21 05:48:00 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
+import { AppProps } from 'next/app'
+
 import '../../styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
